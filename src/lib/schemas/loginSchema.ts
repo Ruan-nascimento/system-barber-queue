8df87d808aa79f2957bfc139529940d105ebb02d
Ruan Carlos <ruan.nascimento019@gmail.com>
2025-04-03 12:49:43 -0300

    fix: re-updates in all tools of system, refactured login and register page and remove all others functions

--- src/lib/schemas/loginSchema.ts
+++ src/lib/schemas/loginSchema.ts
@@ -1,10 +1,19 @@
-import { z } from "zod";
+import { z } from "zod"
 
 export const loginSchema = z.object({
   identifier: z
     .string()
     .min(1, "Email ou telefone é obrigatório")
-    .email("Por favor, insira um email válido"),
+    .refine(
+      (value) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const phoneRegex = /^\(\d{2}\)\s\d{1}\s\d{4}-\d{4}$|^\d{10,11}$/;
+        return emailRegex.test(value) || phoneRegex.test(value)
+      },
+      {
+        message: "Por favor, insira um email ou telefone válido",
+      }
+    ),
   password: z
     .string()
     .min(8, "A senha deve ter no mínimo 8 caracteres")
@@ -13,6 +22,6 @@ export const loginSchema = z.object({
       /^[a-zA-Z0-9]+$/,
       "A senha não pode conter caracteres especiais, apenas letras e números"
     ),
-});
+})
 
-export type LoginForm = z.infer<typeof loginSchema>;
+export type LoginForm = z.infer<typeof loginSchema>
